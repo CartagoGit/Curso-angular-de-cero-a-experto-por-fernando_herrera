dee3deb98b150e882ad5ef92c8569f3c49401f93
Cartago <cartago.relaxingcup@gmail.com>
2022-08-20 16:55:35 +0200

    Dejamos listas las rutas de las paginas de PaisesApp

---
+++ 04_paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [],
+})
+export class PorPaisComponent {
+  termino: string = 'Hola Mundo';
+
+  constructor() {}
+
+  buscar() {
+    console.log("buscar", this.termino);
+  }
+}
